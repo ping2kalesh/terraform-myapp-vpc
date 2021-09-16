ae10cc687a82c45e39f62f571e7aaf06788ea120
Kalesha Shaik <kalesha.shaik@datavail.com>
2021-09-16 10:04:31 +0530

    adds myapp vpc terraform code

---
+++ main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "3.58.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "ap-south-1"
+}
+
+resource "aws_vpc" "myapp-vpc" {
+    cidr_block = "192.168.0.0/16"
+    tags = {
+      "Name" = "myapp-vpc"
+    }
+}
+
+resource "aws_subnet" "myapp-pub-subnet" {
+    cidr_block = "192.168.0.0/24"
+    vpc_id = aws_vpc.myapp-vpc.id
+    tags = {
+      "Name" = "myapp-pub-subnet"
+    }
+
+}
